5c5804d31fdac23402fc4dbb1b1a358b0ab59329
bryan <whompum101@gmail.com>
2019-09-22 10:01:00 -0400

    Rn class

--- CommonUI/src/main/java/com/whompum/commonui/BindableViewHolder.kt
+++ CommonUI/src/main/java/com/whompum/commonui/BindableViewHolder.kt
@@ -3,7 +3,7 @@ package com.whompum.commonui
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class ListViewHolder<T>(v: View): RecyclerView.ViewHolder(v), Bindable<T> {
+class BindableViewHolder<T>(v: View): RecyclerView.ViewHolder(v), Bindable<T> {
     override fun bind(data: T) {
     }
 }
