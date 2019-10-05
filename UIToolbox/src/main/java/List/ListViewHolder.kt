cd09b64bb296e3ef860d9f9d67c320cfcc97d643
bryan <whompum101@gmail.com>
2019-10-05 10:30:19 -0400

    Rm class that s houldn't be here - Left from merge conflict

--- UIToolbox/src/main/java/List/ListViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.whompum.UIToolbox.List
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-/**
- * Generic Base class for a [RecyclerView.Adapter]
- */
-abstract class ListViewHolder<T>(v: View) : RecyclerView.ViewHolder(v), Bindable<T>
