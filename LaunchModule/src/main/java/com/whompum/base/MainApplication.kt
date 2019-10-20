22085a3865c13334e33900a606df03164c05deec
bryan <whompum101@gmail.com>
2019-10-20 11:23:44 -0400

    Rn Base module to LaunchModule to avoid conflicts with existing module name conventions;  Create application class

---
+++ LaunchModule/src/main/java/com/whompum/base/MainApplication.kt
@@ -0,0 +1,17 @@
+package com.whompum.base
+
+import android.app.Application
+
+/**
+ * Base [Application] class for this project.
+ * This includes configuration for [Timber] and [Dagger].
+ */
+class MainApplication: Application() {
+
+    /*override fun onCreate() {
+        super.onCreate()
+        //Configure Timber here
+        //Configure Dagger component here
+    }*/
+
+}
