f26bd2e3389f79a57d27826efb939ca070081f98
bryan <whompum101@gmail.com>
2019-12-15 07:21:56 -0500

    Log with timber

--- App/src/main/java/com/whompum/app/LaunchPresenter.kt
+++ App/src/main/java/com/whompum/app/LaunchPresenter.kt
@@ -3,10 +3,14 @@ package com.whompum.app
 import com.whompum.app.BuildConfig.ADMIN_PASSWORD
 import com.whompum.app.BuildConfig.ADMIN_USERNAME
 import com.whompum.app.BuildConfig.USE_ADMIN_ACCOUNT
+import timber.log.Timber
 
 class LaunchPresenter : LaunchContract.LaunchPresenter() {
 
     override fun validateUsernameAndPassword(username: String, password: String) {
+
+        Timber.i("Username: $username, Password: $password entered. USE_ADMIN_ACCOUNT: $USE_ADMIN_ACCOUNT")
+
         if (USE_ADMIN_ACCOUNT)
             view?.onCredentialsValidated(
                 username,
