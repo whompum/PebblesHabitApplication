ec14b24e12643e8a6b10630738cf8d8633d788c8
whompum101 <whompum101@gmail.com>
2020-01-11 16:03:33 -0500

    Add support for dynamic state setting on drawable

--- UIToolbox/src/main/java/com/whompum/uitoolbox/PriorityStatefullImageView.kt
+++ UIToolbox/src/main/java/com/whompum/uitoolbox/PriorityStatefullImageView.kt
@@ -2,26 +2,55 @@ package com.whompum.uitoolbox
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.ImageView
 import com.whompum.models.Habit.HabitPriority
-import com.whompum.models.Habit.HabitPriority.STATE_NORMAL_UNCOMPLETED
+import com.whompum.models.Habit.HabitPriority.*
 
 /**
  * An [ImageView] that manages custom states
  * to change its background according to a [HabitPriority] state.
  * TODO Setup XML based state setting.
  */
-class PriorityStatefullImageView(val ctx: Context, val attrs: AttributeSet): ImageView(ctx, attrs) {
+class PriorityStatefullImageView(ctx: Context, attrs: AttributeSet) : ImageView(ctx, attrs) {
 
-    var priority: HabitPriority = STATE_NORMAL_UNCOMPLETED
-    set (p) {
-        field = p
-        //Notify Listeners
+    /**
+     * Default value holding containing the current
+     * [R.attr] state
+     */
+    var priorityStateInternal: IntArray = IntArray(1)
+
+    override fun onCreateDrawableState(extraSpace: Int): IntArray {
+        val space =
+            super.onCreateDrawableState(extraSpace + R.styleable.habit_priority_state.size)
+
+        @Suppress("USELESS_ELVIS")
+        View.mergeDrawableStates(
+            space, priorityStateInternal ?: intArrayOf(priorityMapping.getValue(DEFAULT_STATE))
+        )
+
+        return space
+    }
+
+    fun setPriority(priority: HabitPriority) {
+        priorityStateInternal[0] = priorityMapping.getValue(priority)
         refreshDrawableState()
     }
 
-    override fun onCreateDrawableState(extraSpace: Int) = mergeDrawableStates(
-            super.onCreateDrawableState(extraSpace), R.styleable.habit_priority_state
-    )
+    companion object {
+
+        val DEFAULT_STATE = STATE_HIGH_UNCOMPLETED
+
+        /**
+         * Priority mappings between external [HabitPriority]
+         * and attrs declared at [R.styleable.habit_priority_state]
+         */
+        private val priorityMapping = mapOf(
+            STATE_NORMAL_UNCOMPLETED to R.attr.state_priority_normal_uncompleted,
+            STATE_NORMAL_COMPLETED to R.attr.state_priority_normal_completed,
+            STATE_HIGH_UNCOMPLETED to R.attr.state_priority_high_uncompleted,
+            STATE_HIGH_COMPLETED to R.attr.state_priority_high_completed
+        )
+    }
 
 }
