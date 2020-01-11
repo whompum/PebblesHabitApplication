9907387b0e9baf1f61680027f5d36084d2e15617
whompum101 <whompum101@gmail.com>
2020-01-11 09:11:23 -0500

    Plant timber logging tree on 'enableLogs' flag

--- App/src/main/java/com/whompum/app/MainApplication.kt
+++ App/src/main/java/com/whompum/app/MainApplication.kt
@@ -2,17 +2,21 @@ package com.whompum.app
 
 import android.app.Application
 import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 /**
  * Base [Application] class for this project.
  * This includes configuration for [Timber] and [Dagger].
+ * TODO Setup Prod Timber Tree
  */
 class MainApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        //if (BuildConfig.enableLogs)
-            //Timber.plant(Timber.asTree())
+        if (BuildConfig.enableLogs)
+            Timber.plant(DebugTree())
+
     }
 
 }
