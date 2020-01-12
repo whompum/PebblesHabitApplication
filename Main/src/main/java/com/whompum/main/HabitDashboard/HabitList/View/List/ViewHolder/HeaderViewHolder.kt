6b8ae72a71c9c9fe9f8d469482cc5ed72c6e29ec
whompum101 <whompum101@gmail.com>
2020-01-11 21:10:47 -0500

    Rm timber log, add todo for tomorrow

--- Main/src/main/java/com/whompum/main/HabitDashboard/HabitList/View/List/ViewHolder/HeaderViewHolder.kt
+++ Main/src/main/java/com/whompum/main/HabitDashboard/HabitList/View/List/ViewHolder/HeaderViewHolder.kt
@@ -8,9 +8,9 @@ import com.whompum.uitoolbox.List.BindableViewHolder
 import com.whompum.uitoolbox.List.HeaderListItem
 import timber.log.Timber
 
+//TODO Setup onclick to toggle children visibility
 class HeaderViewHolder(v: View): BindableViewHolder<HeaderListItem<HabitListItem>>(v) {
     override fun bind(data: HeaderListItem<HabitListItem>) {
-        Timber.d("bind")
         itemView.findViewById<TextView>(R.id.header_title)
             .text = data.headerTitle
     }
